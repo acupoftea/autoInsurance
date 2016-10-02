caa9da350e7ed7929b76b490af73e36f9712ac3d
dahai liu <dahai liu>
2016-10-02 22:34:22 +0800

    车险算费web服务c#测试工具

---
+++ testAutoInsuranceWS/testAutoInsuranceWS/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace testAutoInsuranceWS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "登录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "查询基本信息";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 448);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 38);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "保存&&核保";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 536);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 38);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "投保单号查保单号";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 360);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(158, 38);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "算费";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "京NWG898";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(378, 365);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 25);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(186, 545);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(328, 25);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "50119003900208891897";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(250, 370);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 19);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = "商业";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(186, 370);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 19);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.Text = "交强";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(314, 370);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(58, 19);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "一起";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(186, 457);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(328, 25);
+            this.textBox3.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "web服务地址";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 580);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(158, 38);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "查询保险公司列表";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 100);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(758, 254);
+            this.textBox4.TabIndex = 15;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 404);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(158, 38);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "查询关系人信息";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(186, 413);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(328, 25);
+            this.textBox5.TabIndex = 17;
+            this.textBox5.Text = "430121198002032019";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(186, 501);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(328, 25);
+            this.textBox6.TabIndex = 19;
+            this.textBox6.Text = "TDAA201611020000715042";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 492);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(158, 38);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "查询核保意见";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(282, 65);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(232, 25);
+            this.textBox7.TabIndex = 20;
+            this.textBox7.Text = "LFV2A2BS4F4079151";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(520, 65);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 25);
+            this.textBox8.TabIndex = 21;
+            this.textBox8.Text = "GA6696";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(187, 580);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 721);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "车险web服务测试工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ testAutoInsuranceWS/testAutoInsuranceWS/Form1.cs
@@ -0,0 +1,177 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace testAutoInsuranceWS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //ws.Url = "http://121.196.222.85:9081/autoInsurance?wsdl";
+        }
+
+        testAutoInsuranceWS.localhost.CommandCenterService ws = new localhost.CommandCenterService();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string rtn = ws.login("{\"insuranceId\": 2, \"ukey\":\"JJ115192BJ\",\"loginName\":\"LGXY-00001\",\"password\":\"RKrv5D61\"}");
+                MessageBox.Show(rtn);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox4.Text = "";
+                string rtn = ws.queryBaseData("{\"insuranceId\": 1, \"chepNu\":\"" + textBox1.Text + "\", \"chejh\":\"" + textBox7.Text + "\", \"fadjh\":\"" + textBox8.Text + "\"}");
+                Console.WriteLine(rtn);
+                textBox4.Text = rtn;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        class SuanfRtnData
+        {
+            public string saveHebId { set; get; }
+            public bool success { set; get; }
+            public string message { set; get; }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int jiaoqOrShangy = -1;
+
+            if (radioButton1.Checked)
+                jiaoqOrShangy = 1;
+            if (radioButton2.Checked)
+                jiaoqOrShangy = 0;
+            if (radioButton3.Checked)
+                jiaoqOrShangy = 2;
+
+            try
+            {
+                string _in = "{\"insuranceId\": 2, \"carBaseInfo\":[" + textBox4.Text + "], \"jiaoqOrShangy\":" + jiaoqOrShangy + ", \"jiaoqStartDate\":\"" + dateTimePicker1.Text + "\",\"xianZDetail\":[{\"xianZ\":\"A\",\"bujmP\":true,\"checked\":true},{\"xianZ\":\"B\",\"baoE\":1000000,\"bujmP\":true,\"checked\":true},{\"xianZ\":\"D11\",\"baoE\":10000,\"bujmP\":true,\"checked\":true},{\"xianZ\":\"D12\",\"baoE\":10000,\"bujmP\":true,\"checked\":true},{\"xianZ\":\"G\",\"bujmP\":true,\"checked\":true},{\"xianZ\":\"F\",\"baoE\":10,\"checked\":true},{\"xianZ\":\"X1\",\"bujmP\":true,\"checked\":true},{\"xianZ\":\"Z\",\"bujmP\":true,\"checked\":true},{\"xianZ\":\"L\",\"baoE\":5000,\"bujmP\":true,\"checked\":true}]}";
+                //string _in = "{\"insuranceId\": 1, \"carBaseInfo\":[" + textBox4.Text + "],\"jiaoqOrShangy\":" + jiaoqOrShangy + ", \"jiaoqStartDate\":\"" + dateTimePicker1.Text + "\",\"xianZDetail\":[{\"xianZ\":\"A\",\"bujmP\":true,\"checked\":true},{\"xianZ\":\"B\",\"baoE\":500000,\"bujmP\":true,\"checked\":true},{\"xianZ\":\"G\",\"bujmP\":true,\"checked\":true}]}";
+                Console.WriteLine(_in);
+                string rtn = ws.suanF(_in);
+                SuanfRtnData srd = JsonConvert.DeserializeObject <SuanfRtnData>(rtn);
+                textBox3.Text = srd.saveHebId;
+
+                Console.WriteLine(rtn);
+                MessageBox.Show(rtn);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int jiaoqOrShangy = -1;
+
+            if (radioButton1.Checked)
+                jiaoqOrShangy = 1;
+            if (radioButton2.Checked)
+                jiaoqOrShangy = 0;
+            if (radioButton3.Checked)
+                jiaoqOrShangy = 2;
+
+            try
+            {
+                //chezInfo = "{\"customMobile\":\"\",\"addressCName\":\"北京市朝阳区安慧北里逸园小区26号楼602号\",\"customerCode\":\"1100100000160886\",\"identifyNumber\":\"430121198002032019\",\"customerCName\":\"何超\"}";
+                //string rtn = ws.saveAndHeB("{\"insuranceId\": 1, \"hebXianZ\": \"" + jiaoqOrShangy + "\", \"saveHebId\": \"" + textBox3.Text + "\", relation: [{\"type\":\"cheZ\",\"customMobile\":\"13501235344\",\"addressCName\":\"北京市朝阳区安慧北里逸园小区26号楼602号\",\"customerCode\":\"1100100000160886\",\"identifyNumber\":\"430121198002032019\",\"customerCName\":\"何超\"},{\"type\":\"touBR\",\"customMobile\":\"13501235344\",\"addressCName\":\"北京市朝阳区安慧北里逸园小区26号楼602号\",\"customerCode\":\"1100100000160886\",\"identifyNumber\":\"430121198002032019\",\"customerCName\":\"何超\"},{\"type\":\"beiBXR\",\"customMobile\":\"13501235344\",\"addressCName\":\"北京市朝阳区安慧北里逸园小区26号楼602号\",\"customerCode\":\"1100100000160886\",\"identifyNumber\":\"430121198002032019\",\"customerCName\":\"何超\"}]}");
+                string rtn = ws.saveAndHeB("{\"insuranceId\": 1, \"hebXianZ\": \"" + jiaoqOrShangy + "\", \"saveHebId\": \"" + textBox3.Text + "\", relation: [" + chezInfo + "]}");
+                MessageBox.Show(rtn);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string rtn = ws.queryBaodhByToubdh("{\"insuranceId\": 2, \"toubdh\": \"" + textBox2.Text + "\"}");
+                Console.WriteLine(rtn);
+                MessageBox.Show(rtn);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = ws.Url;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string rtn = ws.queryBaoxgsList();
+                Console.WriteLine(rtn);
+                MessageBox.Show(rtn);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        string chezInfo = "";
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string rtn = ws.queryRelationData("{\"insuranceId\": 2, \"shenfzNu\": \"" + textBox5.Text + "\"}");
+                chezInfo = rtn;
+                Console.WriteLine(rtn);
+                MessageBox.Show(rtn);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string rtn = ws.queryHebJg("{\"insuranceId\": 1, \"toubDh\": \"" + textBox6.Text + "\"}");
+                chezInfo = rtn;
+                Console.WriteLine(rtn);
+                MessageBox.Show(rtn);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+       
+    }
+}
